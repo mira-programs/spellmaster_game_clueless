fef26c9bc2f9293f8c5b8d382608feffd8977a7c
hmoun <144455381+hmoun@users.noreply.github.com>
2023-09-15 16:12:28 +0300

    Add files via upload

--- cluelessDraft01.c
+++ cluelessDraft01.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main()
 {
     FILE *sptr; // file pointer
-    char c;     // reading the characters individually instead of strings because we don't know how long the array length is
+    // char c;     // reading the characters individually instead of strings because we don't know how long the array length is
+    char name1[20]; // scanning the players' names
+    printf("Player one name: ");
+    scanf("%s", name1);
+    char name2[20];
+    printf("\nPlayer two name: ");
+    scanf("%s", name2);
+    int s1 = 5;
+    int s2 = 16;
+    char **a = (char **)malloc(s1 * sizeof(int *));
+    for (int i = 0; i < s1; i++)
+        a[i] = (char *)malloc(s2 * sizeof(int));
     sptr = fopen("spells.txt", "r");
     while (!feof(sptr)) // while pointer is not pointing to the end of the file (file end of file)
     {
-        c = fgetc(sptr);
-        printf("%c", c);
+        for (int i = 0; i < 16; i++)
+            for (int j = 0; j < 5; j++)
+                a[i][j] = fgetc(sptr);
     }
+    for (int i = 0; i < 16; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            printf("%c", a[i][j]);
+        }
+        printf("\n");
+    }
+
     fclose(sptr);
     return 0;
 }
