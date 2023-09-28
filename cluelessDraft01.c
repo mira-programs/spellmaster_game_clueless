2eed93a11147c0d87a6e28b975d0eb57c09638a3
hmoun <144455381+hmoun@users.noreply.github.com>
2023-09-28 12:06:29 +0300

    Add files via upload

--- cluelessDraft01.c
+++ cluelessDraft01.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 int main()
 {
-    FILE *sptr; // file pointer
-    // char c;     // reading the characters individually instead of strings because we don't know how long the array length is
+    FILE *sptr;     // file pointer
+    char c;         // reading the characters individually instead of strings because we don't know how long the array length is
     char name1[20]; // scanning the players' names
     printf("Player one name: ");
     scanf("%s", name1);
@@ -12,15 +12,18 @@ int main()
     scanf("%s", name2);
     int s1 = 5;
     int s2 = 16;
-    char **a = (char **)malloc(s1 * sizeof(int *));
+    char **a = (char **)malloc(s1 * sizeof(char *));
     for (int i = 0; i < s1; i++)
-        a[i] = (char *)malloc(s2 * sizeof(int));
+        a[i] = (char *)malloc(s2 * sizeof(char));
     sptr = fopen("spells.txt", "r");
     while (!feof(sptr)) // while pointer is not pointing to the end of the file (file end of file)
     {
         for (int i = 0; i < 16; i++)
             for (int j = 0; j < 5; j++)
-                a[i][j] = fgetc(sptr);
+            {
+                char line[1000];
+                a[i][j] = fgets(20, sizeof line, sptr);
+            }
     }
     for (int i = 0; i < 16; i++)
     {
@@ -30,7 +33,10 @@ int main()
         }
         printf("\n");
     }
-
+    int i;
+    for (i = 0; i < s1; i++)
+        free(a[i]);
+    free(a);
     fclose(sptr);
     return 0;
 }
