dfd9afee0ceb9b06814b27f02f98d0ddda84ba70
mira-programs <mirahussein06@gmail.com>
2023-11-11 22:56:25 +0300

    Update bot1.c
    'easy' level bot done, but minimal testing has been done at this stage.

--- bot1.c
+++ bot1.c
@@ -2,48 +2,83 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include "BST.h"
-#include "createSpellsTree.h"
 #include "checkMove.h"
 #include "coinToss.h"
 
-//generate random number, used for modified search
-int randomNum(int min, int max){
+// generate random number, used for modified search
+int randomNum(int min, int max)
+{
     srand(time(NULL)); // seed with the current time to get different results at different runs
     int random_number = rand() % (max - min + 1) + min;
     return random_number;
 }
 
-//modified search function
+// modified search function
 node *modifiedSearch(node *root, char prev, int spellsLeft[])
 { // recursive search function to find a spell in the tree
-    if(root == NULL) return NULL; //should never be reached, possible error. might need to add print error
-    if(root->left == NULL && root->right == NULL) return root;
 
-    if (root->spell[0] == prev){
+    if (root == NULL)
+    {
+        fprintf(stderr, "Error: Root is NULL in modifiedSearch function.\n");
+        // You might want to exit the program or handle the error appropriately.
+        exit(EXIT_FAILURE);
+    }
+
+    if (root == NULL)
+        return NULL; // should never be reached, possible error. might need to add print error
+    if (root->left == NULL && root->right == NULL)
+        return root;
+
+    if (root->spell[0] == prev)
+    {
         int randnum;
-        randnum = randomNum(1, spellsLeft[prev - 'a']); //random num to decide which of the spells to use
-        for(int i = 1; i <= randnum; i++){
-            if(root->left == NULL && root->right == NULL) break; //nowhere else to go, pick the root spell
-            if(root->left == NULL) root = root->right; //can't go left, go right
-            else if(root->right == NULL) root = root->left; //can't go right, go left
-            else{ //can go either way, random choice
-                if(coinToss() == 0){
+        randnum = randomNum(1, spellsLeft[prev - 'a']); // random num to decide which of the spells to use
+        for (int i = 1; i <= randnum; i++)
+        {
+            if (root->left == NULL && root->right == NULL)
+                break; // nowhere else to go, pick the root spell
+            if (root->left == NULL)
+                root = root->right; // can't go left, go right
+            else if (root->right == NULL)
+                root = root->left; // can't go right, go left
+            else
+            { // can go either way, random choice
+                if (coinToss() == 0)
+                {
                     root = root->right;
-                }else{
+                }
+                else
+                {
                     root = root->left;
                 }
             }
         }
         return root;
-    }else if (root->spell[0] > prev)
+    }
+    else if (root->spell[0] > prev)
         return modifiedSearch(root->left, prev, spellsLeft);
     else
         return modifiedSearch(root->right, prev, spellsLeft);
 }
 
-int main(){
-    int spellsLeft [26] = {0};
-    node* root = createAndPrintTree("spells.txt", spellsLeft);
+int main()
+{
+    int spellsLeft[26] = {0};
 
+    node *root = createAndPrintTree("spells.txt", spellsLeft);
+    
+    printf("\ncurrent spells count:\n");
+    for (int i = 0; i < 26; i++)
+    {
+        printf("Count for '%c': %d\n", 'a' + i, spellsLeft[i]);
+    }
+    
+    node* move = modifiedSearch(root, 'q', spellsLeft);
+    checkMove(root, 'q', move->spell, spellsLeft);
+
+    printf("\nend of game spells count:\n");
+    for (int i = 0; i < 26; i++)
+    {
+        printf("Count for '%c': %d\n", 'a' + i, spellsLeft[i]);
+    }
 }
