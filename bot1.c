0fcd0f8d8bedf7180353b44efd03a3f602653b84
mira-programs <mirahussein06@gmail.com>
2023-11-11 22:36:38 +0300

    Update bot1.c

--- bot1.c
+++ bot1.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include "BST.h"
+#include "createSpellsTree.h"
+#include "checkMove.h"
+#include "coinToss.h"
 
-//TESTING STAGE - NOT A BOT YET
+//generate random number, used for modified search
+int randomNum(int min, int max){
+    srand(time(NULL)); // seed with the current time to get different results at different runs
+    int random_number = rand() % (max - min + 1) + min;
+    return random_number;
+}
+
+//modified search function
+node *modifiedSearch(node *root, char prev, int spellsLeft[])
+{ // recursive search function to find a spell in the tree
+    if(root == NULL) return NULL; //should never be reached, possible error. might need to add print error
+
+
+    if (root->spell[0] == prev){
+        int randnum;
+        randnum = randomNum(1, spellsLeft[prev - 'a']); //random num to decide which of the spells to use
+        
+
+    }
+
+    if (root->spell[0] > prev)
+        return modifiedSearch(root->left, prev);
+    else
+        return modifiedSearch(root->right, prev);
+}
+
 int main(){
-    int alphabet [26] = {0};
-    char word []= "hello";
-    char firstLetter = word[0];
-    // Check if the first letter is a lowercase letter
-    if ('a' <= firstLetter && firstLetter <= 'z') {
-        // Update the count in the alphabet array
-        alphabet[firstLetter - 'a']++;
-    }
-    for (int i = 0; i < 26; i++) {
-        printf("Count for '%c': %d\n", 'a' + i, alphabet[i]);
-    }
+    int spellsLeft [26] = {0};
+    node* root = createAndPrintTree("spells.txt", spellsLeft);
+
 }
