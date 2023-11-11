f1ec0eb4fa2bed092c5f782439383dbfbf596212
mira-programs <mirahussein06@gmail.com>
2023-11-11 22:46:11 +0300

    Update bot1.c
    adding modified search for bot, incomplete

--- bot1.c
+++ bot1.c
@@ -18,19 +18,28 @@ int randomNum(int min, int max){
 node *modifiedSearch(node *root, char prev, int spellsLeft[])
 { // recursive search function to find a spell in the tree
     if(root == NULL) return NULL; //should never be reached, possible error. might need to add print error
-
+    if(root->left == NULL && root->right == NULL) return root;
 
     if (root->spell[0] == prev){
         int randnum;
         randnum = randomNum(1, spellsLeft[prev - 'a']); //random num to decide which of the spells to use
-        
-
-    }
-
-    if (root->spell[0] > prev)
-        return modifiedSearch(root->left, prev);
+        for(int i = 1; i <= randnum; i++){
+            if(root->left == NULL && root->right == NULL) break; //nowhere else to go, pick the root spell
+            if(root->left == NULL) root = root->right; //can't go left, go right
+            else if(root->right == NULL) root = root->left; //can't go right, go left
+            else{ //can go either way, random choice
+                if(coinToss() == 0){
+                    root = root->right;
+                }else{
+                    root = root->left;
+                }
+            }
+        }
+        return root;
+    }else if (root->spell[0] > prev)
+        return modifiedSearch(root->left, prev, spellsLeft);
     else
-        return modifiedSearch(root->right, prev);
+        return modifiedSearch(root->right, prev, spellsLeft);
 }
 
 int main(){
