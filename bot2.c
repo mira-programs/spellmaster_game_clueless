36a63b9d808ccb8b9867a9150b89c9893c8396f0
mms161 <144433947+mms161@users.noreply.github.com>
2023-11-12 09:04:08 +0200

    Starting moderately hard version of bot

---
+++ bot2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "freeTree.h"
+#include "coinToss.h"
+
+//moderate bot: only check opponent's move.
+//will be copied into bot1 once it works.
+
+//make the bot choose a spell
+void botMakesMove(node *spellTree, char previousChar, char *spell, int spellsLeft[]) {
+    if (previousChar == ' ') {      //if the bot starts previousChar would be ' ' as we've initialized it to be.
+        
+    }
+    else {  //if previousChar isn't ' ', means it was changed and therefore the bot has to take it into consideration when making its next move.
+
+    }
+};
